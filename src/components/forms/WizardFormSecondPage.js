a29ca32b5618a24b080e522a33658a1aabe6e0dc
Matt Gilarmo <mgilarmo@gmail.com>
2019-05-13 12:39:29 -0400

    working on dynamic input

--- src/components/forms/WizardFormSecondPage.js
+++ src/components/forms/WizardFormSecondPage.js
@@ -1,33 +1,56 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import validate from './validate';
-import renderInput from './renderInput';
 
 
-const WizardFormSecondPage = props => {
-  const { handleSubmit, previousPage } = props
-  return (
-    <div>
-      <label className="add-input-field">
-        <i className="fa fa-plus" /> Add another ingredient
-      </label>
-      <form onSubmit={handleSubmit}>
-        <Field 
-          name="ingredients" 
-          type="text" 
-          component={renderInput} 
-          label="Ingredients" />
-        <div>
-          <button type="button" className="previous" onClick={previousPage}>
-            Previous
-          </button>
-          <button type="submit" className="next">
-            Next
-          </button>
+class WizardFormSecondPage extends React.Component {
+  state = {
+    ingred: []
+  };
+
+  addIngredient() {
+    this.setState({ingred: [...this.state.ingred]})
+  }
+
+  changeInput(e, i) {
+    this.state.ingred[i] = e.target.value
+    this.setState({ingred: this.state.ingred})
+  }
+
+  dynamicInput = () => {
+    {this.state.ingred.map((item, i) => {
+      return (
+        <div key={i}>
+          <input value={item} onChange={e => this.changeInput(e, i)}/>
         </div>
-      </form>
-    </div>
-  )
+      );
+    })}
+  }
+
+  render() {
+    return (
+      <div>
+        <label className="add-input-field" onClick={e => this.addIngredient(e)}>
+          <i className="fa fa-plus" /> Add another ingredient
+        </label>
+        <form onSubmit={this.props.handleSubmit}>
+          <Field 
+            name="ingredients" 
+            type="text" 
+            component={this.state.ingred} 
+            label="Ingredients" />
+          <div>
+            <button type="button" className="previous" onClick={this.props.previousPage}>
+              Previous
+            </button>
+            <button type="submit" className="next">
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default reduxForm({
